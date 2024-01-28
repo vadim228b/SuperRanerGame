833bc5c62d780b2717fcb9c558d8cfd5bdc1a6cd
vadim228b <149076544+vadim228b@users.noreply.github.com>
2024-01-28 17:32:36 +0300

    Game v0.0.6

---
+++ Assets/C#/VolumeValue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValue : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = musicVolume;
+    
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+}
+
+
+
