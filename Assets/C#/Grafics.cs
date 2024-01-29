ae0e0baa70d1860ccc38826a28ea09a024cf1c9b
vadim228b <149076544+vadim228b@users.noreply.github.com>
2024-01-29 19:03:15 +0300

    Game v0.0.7

---
+++ Assets/C#/Grafics.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grafics : MonoBehaviour
+{ 
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+
+
+
+}
